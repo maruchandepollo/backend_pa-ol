0a7760f5ae3e2801ac0144c2363e9f15e086e74e
Bodega <shispa_gb@live.cl>
2026-05-04 15:17:22 -0400

    init backend panol

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend funcionando");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en puerto 3000");
+});
